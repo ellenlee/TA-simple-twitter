41c76fa735d2fc11fc8c2bf937284efae999b2ea
Sam Lin <greenlin111@gmail.com>
2018-01-31 11:55:59 +0800

    finish index spec

--- spec/requests/tweet_spec.rb
+++ spec/requests/tweet_spec.rb
@@ -1,18 +1,52 @@
 RSpec.describe 'Tweet', type: :request do
-  let(:user) { create(:user) }
-  describe '#index' do
-    it 'will redirect to log in page if not login' do
-      get tweets_path
-      expect(response).to redirect_to(new_user_session_path)
-      expect(response).to have_http_status(302)
+  let(:user) { create(:user, email: FFaker::Internet.email, name: 'no_tweets') }
+  let(:user_with_tweets) { create(:user_with_tweets) }
+
+  context '#index' do
+    describe 'user not login' do
+      it 'will redirect to log in page' do
+        get tweets_path
+        expect(response).to redirect_to(new_user_session_path)
+        expect(response).to have_http_status(302)
+      end
     end
 
-    it 'will show all tweets if log in' do
-      sign_in(user)
-      get tweets_path
-      expect(response).to have_http_status(200)
-      expect(controller.current_user).to eq(user)
+    context 'user log in' do
+      before do
+        user
+        user_with_tweets
+        Followship.create(user_id: user.id, following_id: user_with_tweets.id)
+        sign_in(user)
+        get tweets_path
+      end
+
+      it 'can render index' do
+        expect(response).to render_template(:index)
+      end
+
+      describe 'user behaviour' do
+        it 'show current_user as log in user' do
+          expect(controller.current_user).to eq(user)
+        end
+
+        it 'can show all popular user' do
+          expect(assigns(:users).first).to eq(user_with_tweets)
+        end
+      end
+
+      describe 'tweets behaviour' do
+        it 'will show all tweets' do
+          expect(response).to have_http_status(200)
+        end
+
+        it 'can see all tweets instance' do
+          expect(assigns(:tweets)).to eq(Tweet.all)
+        end
+
+        it 'have tweet instance' do
+          expect(assigns(:tweet)).not_to be nil
+        end
+      end
     end
   end
-
 end
