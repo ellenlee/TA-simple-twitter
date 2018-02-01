de967042d1e564857f79b0e61dd676a4945cde80
Ellen Lee <leechinghui.tw@gmail.com>
2018-02-01 17:47:24 +0800

    Uninstall mini_magick

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -14,20 +14,20 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
+  # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
-     ActionController::Base.helpers.asset_path("fallback/" +["avatar.png"].compact.join('_'))
+     # ActionController::Base.helpers.asset_path("fallback/" +["avatar.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  end
+  # end
 
-  version :thumb do
-    process resize_to_fit: [50, 50]
-  end
-
-  version :medium do
-    process resize_to_fit: [150, 150]
-  end
+  # version :thumb do
+  #   process resize_to_fit: [50, 50]
+  # end
+  #
+  # version :medium do
+  #   process resize_to_fit: [150, 150]
+  # end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
